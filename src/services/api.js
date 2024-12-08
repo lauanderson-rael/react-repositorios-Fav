c83125f54f55feb9559329ff848bbcc1bb3f96d4
Lauanderson Rael <100454015+lauanderson-rael@users.noreply.github.com>
2024-12-07 22:16:03 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: 'https://api.github.com',
+      baseURL: import.meta.env.VITE_API_URL,
     // headers: {
     //     Authorization: `token ${token}` // Adiciona o token globalmente
     // }
